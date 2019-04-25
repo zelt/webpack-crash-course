1606b12a5417f01d04f871f987e7a35a31839831
zelt <michi74@gmail.com>
2019-04-25 17:54:05 +0900

    implement getting started

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import _ from 'lodash'
+
+function component() {
+	const element = document.createElement('div');
+	const array = ['Hello','webpack']
+	element.innerHTML = _.join(array, ' ')
+	return element;
+}
+
+document.body.appendChild(component());
